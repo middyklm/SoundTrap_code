fc869e0a1e905cf2e6e9b2266835ece05536a4df
Middy <middy.klm89@gmail.com>
2022-07-14 14:22:31 +1000

    Add files via upload

---
+++ SoundTrap Main App/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/Users/Middy/workspace_v8/cslVC5505/Release/cslVC5505.lib" -llibc.a
+
